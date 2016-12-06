5f487990165043c73c860952bb15b8332c9c6e4c
ssukumar <shruthi.sukumar@colorado.edu>
2016-12-06 12:17:11 -0700

    Shrinking error appears stochastically

--- final_mcmc_net.py
+++ final_mcmc_net.py
@@ -20,9 +20,7 @@ class Data:
 
         trainData = trainData.replace(-1,np.nan)
         self.features = trainData.shape[1];
-
         trainData = trainData.fillna(trainData.mean())
-
         self.trainInputs = trainData.as_matrix()
         self.trainTargets = trainLabel.as_matrix()
         [self.trainInputs, self.valInputs, self.target, self.valTargets] = train_test_split(self.trainInputs,self.trainTargets ,test_size = self.valRatio)
@@ -31,6 +29,12 @@ class Data:
         self.trainStd = np.array(list(tnStd))
         self.input = z_score_inputs(self.trainInputs,self.trainMean,self.trainStd);
         self.valInputs = z_score_inputs(self.valInputs,self.trainMean,self.trainStd);
+        
+    def select_subset(self, start, number):
+        d = Data('./data/train.csv','./data/train_labels.txt',0.30)
+        d.input = self.input[start:start+number,:]
+        d.target = self.target[start:start+number,:]
+        return d
 
 def z_score_inputs(Inputs, Mean, StdDev):
     """ 
@@ -44,8 +48,8 @@ class Weights:
     
     def __init__(self,input_dim = 451, hidden_neurons = 32, output_dim =1):
         # Sets gaussian prior to weight vector
-        self.Wl1 = np.random.normal(0, 1, (input_dim, hidden_neurons))
-        self.Wl2 = np.random.normal(0, 1, (hidden_neurons, output_dim))
+        self.Wl1 = np.random.normal(0, .1, (input_dim, hidden_neurons))
+        self.Wl2 = np.random.normal(0, .1, (hidden_neurons, output_dim))
         
 
 def sigmoid(input, w):
@@ -85,18 +89,30 @@ def obs_llh(w_vec, idx, layer, data_obj, w_obj, tau):
 
     mu = predict(w_obj, data_obj)
     s = np.sqrt(1/tau)
+#     print data_obj.target/np.exp(mu)
+    print data_obj.target.shape
+    print (data_obj.target/np.exp(mu))
     return lognorm.logpdf(data_obj.target/np.exp(mu), s)
     
     
-def mcmc_fit(data_obj, w_obj, tau, mcmc_iter = 1000, sigma0=1):
+def mcmc_fit(data_obj, w_obj, tau, prior_a, prior_b, mcmc_iter = 1000, sigma0=.1):
     
     for iter in range(mcmc_iter):
         ct = 0
+        print iter
         for w_mat in [w_obj.Wl1, w_obj.Wl2]:
             ct+=1 
             for neuron in range(w_mat.shape[0]):
                 prior = np.random.normal(0, sigma0, w_mat.shape[1])
+#                 print prior.shape
+#                 print w_mat[neuron,:].shape
+                print "layer = %d" %ct
+                print neuron
+                print w_mat[neuron,:]
+#                 print obs_llh(w_mat[neuron,:],neuron, ct, data_obj, w_obj, tau)
                 w_mat[neuron,:], next_llh = elliptical_slice(w_mat[neuron,:],prior,obs_llh,pdf_params=(neuron, ct, data_obj, w_obj, tau),cur_lnpdf=None,angle_range=None)
+#                 print w_mat[neuron,:]
+#                 print tau
         tau, llh_val = slice_update(tau, tau_llh, bounds = [0,10],pdf_params=(prior_a, prior_b, data_obj, w_obj))
         
     return w_obj, tau
@@ -104,12 +120,17 @@ def mcmc_fit(data_obj, w_obj, tau, mcmc_iter = 1000, sigma0=1):
         
 def main():
 
-    tau = tau_prior(2,2)
+    pri_a = 2
+    pri_b = 2
+    tau = tau_prior(pri_a, pri_b)
     w_obj = Weights()
     data_obj = Data('./data/train.csv','./data/train_labels.txt',0.30);
     data_obj.inputPreprocess();
     
-    w_obj, tau = mcmc_fit(data_obj, w_obj, tau)
+    for i in range(data_obj.input.shape[0]):
+        d_obj = data_obj.select_subset(i,1)
+        w_obj, tau = mcmc_fit(d_obj, w_obj, tau, pri_a, pri_b)
+        
     val_pred = predict(w_obj, data_obj.valInputs)
     val_loss = mean_squared_error(data.obj.valTargets,val_pred)
     return val_loss
